c61b541f4d732c7d91a98bf49ab07d8da49b607d
maxkoval <maxkovalburn@gmail.com>
2017-12-17 04:06:19 +0200

    backend

--- app/js/scripts/scripts.js
+++ app/js/scripts/scripts.js
@@ -1,16 +1,88 @@
 $(document).ready(function () {
     "use strict";
 
-    var clock = $('#counter').FlipClock(1, {
-        clockFace: 'Counter',
-        countdown: true,
-        minimumDigits: 6
+    var url = "https://script.google.com/macros/s/AKfycbwfhBk8oMHAn5Vrjcje6t5HI0raeNs05VqvybEkN0TmUqypuRI/exec",
+        clock,
+        current = 0;
+
+    $.ajax({
+        type: "POST",
+        data: {},
+        url: url,
+        dataType: "json",
+        success: function (data) {
+            if (data) {
+                var real = data['Реальна кількість'],
+                    total = data['Додана кількість'],
+                    all = real + total;
+
+                current = all;
+
+                clock = $('#counter').FlipClock(all, {
+                    clockFace: 'Counter',
+                    countdown: true,
+                    minimumDigits: 6
+                });
+            }
+        }
     });
 
-    setTimeout(function() {
-        setInterval(function() {
-            clock.increment();
-        }, 3000);
-    })
+    setInterval(function () {
+        $.ajax({
+            type: "POST",
+            data: {},
+            url: url,
+            dataType: "json",
+            success: function (data) {
+                if (data) {
+                    var real = data['Реальна кількість'],
+                        total = data['Додана кількість'],
+                        all = real + total;
 
+                    if (all > current) {
+                        var diff = all - current;
+
+                        for (var i = 0; i < diff; i++) {
+                            clock.increment();
+                        }
+                    }
+                }
+            }
+        });
+    }, 15000);
+
+    $(".button").on('click', function (event) {
+        event.preventDefault();
+
+        $.ajaxSetup({cache: true});
+        $.getScript('//connect.facebook.net/uk_UA/sdk.js', function () {
+            FB.init({
+                appId: '2253326001359487',
+                version: 'v2.3'
+            });
+            FB.ui({
+                    method: 'share',
+                    title: 'Я СВЯТКУЮ ВІДПОВІДАЛЬНО',
+                    description: '#СВЯТКУЙНЕРУЛЮЙ #СВЯТКУЙВІДПОВІДАЛЬНО #ВДОМАЧЕКАЮТЬ',
+                    href: window.location.href,
+                },
+                function (response) {
+                    if (response && !response.error_code) {
+                        clock.increment();
+
+                        current++;
+
+                        $.ajax({
+                            type: "GET",
+                            data: {},
+                            url: url,
+                            dataType: "json",
+                            success: function () {
+                                //success function
+                            }
+                        });
+                    }
+                });
+        });
+    });
 });
